ef093c0f2fc0d60d8c7cdd50dff12c5b750d7075
nikibelyy <ogurtsov.1999@gmail.com>
2025-09-01 00:44:11 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ddr-v1').then(cache => cache.addAll([
-    './', './index.html', './styles.css', './app.js', './manifest.json'
-  ])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
