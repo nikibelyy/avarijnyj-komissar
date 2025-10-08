ef092e6f77a664726b46dfddf80b6dcb5245f767
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-08 05:58:02 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -4,20 +4,21 @@ function init() {
     // Создаем карту, центрируем на Воронеж
     var myMap = new ymaps.Map("map", {
         center: [51.6833, 39.1802], // координаты центра Воронежа
-        zoom: 13,                    // подходящий масштаб для города
-        controls: []
+        zoom: 13,                   // масштаб
+        controls: [],
+        type: 'yandex#dark'         // <<< НОЧНАЯ ТЕМА
     });
 
-    // Создаем глобальный провайдер пробок с включенными дорожными событиями
+    // Глобальный провайдер пробок
     window.actualProvider = new ymaps.traffic.provider.Actual(
         {},
-        { infoLayerShown: true } // дорожные события
+        { infoLayerShown: true }
     );
 
     // Добавляем провайдер на карту
     window.actualProvider.setMap(myMap);
 
-    // Слой инфоточек для событий на дорогах
+    // Слой инфоточек для событий
     var infoLayer = new ymaps.traffic.InfoLayer(myMap, {
         provider: window.actualProvider
     });
